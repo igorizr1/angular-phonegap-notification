0886fb81307c01259b59754cc8172af0626520da
codestack <codestack123@gmail.com>
2014-06-30 17:10:24 +0300

    initial

---
+++ NotificationService.js
@@ -0,0 +1,31 @@
+angular.module('NotificationService', ["general.config"])
+.factory('NotificationService', function(GENERAL_CONFIG) {
+    return {
+        alert : function(message){
+            try{
+                return navigator.notification
+                    .alert(message,
+                        function(){},
+                        GENERAL_CONFIG.APP_NAME,
+                        "ok");
+            }catch(t){
+                return alert(message);
+            }
+        },
+        confirm : function(message, callback){
+            try{
+                navigator.notification.confirm(
+                    message,
+                    function(buttonIndex){
+                        if(callback)callback(buttonIndex === 1 ? true : false);
+                    },
+                    GENERAL_CONFIG.APP_NAME,
+                    ['Ok','Cancel']
+                );
+            }catch(t){
+                confirm(message, callback);
+            }
+        }
+    };
+    
+});
